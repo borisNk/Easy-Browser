636503eed746d87d5d817914287acb59e19f6298
borisNk <borisnk@icloud.com>
2020-02-19 20:44:15 +0100

    Initial Commit.

--- Easy Browser/ViewController.swift
+++ Easy Browser/ViewController.swift
@@ -6,15 +6,25 @@
 //  Copyright © 2020 Boris Nikolaev Borisov. All rights reserved.
 //
 
+import WebKit
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
 
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let url = URL(string: "https://www.apple.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
-
 
 }
 
